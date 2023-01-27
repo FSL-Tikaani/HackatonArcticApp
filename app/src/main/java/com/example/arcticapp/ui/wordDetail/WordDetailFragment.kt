d27cbdfba03cba1471bee015b340e31bbfe568ef
artemObrazumov <a.obraz60022006@gmail.com>
2023-01-27 22:48:23 +0300

    added audio player

--- app/src/main/java/com/example/arcticapp/ui/wordDetail/WordDetailFragment.kt
+++ app/src/main/java/com/example/arcticapp/ui/wordDetail/WordDetailFragment.kt
@@ -10,6 +10,8 @@ import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.arcticapp.data.models.WordModel
 import com.example.arcticappfinal.databinding.FragmentWordDetailBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 
 class WordDetailFragment(
     private val word: String
@@ -19,6 +21,7 @@ class WordDetailFragment(
     }
     private lateinit var binding: FragmentWordDetailBinding
     private lateinit var viewModel: WordDetailViewModel
+    private lateinit var audioPlayer: ExoPlayer
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,14 +29,24 @@ class WordDetailFragment(
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentWordDetailBinding.inflate(inflater)
+        audioPlayer = createAudioPlayer()
         dialog!!.window!!.requestFeature(Window.FEATURE_NO_TITLE)
         viewModel = ViewModelProvider(this)[WordDetailViewModel::class.java]
         viewModel.loadWord(word)
         binding.close.setOnClickListener {
             dismiss()
         }
+        binding.listenButton.setOnClickListener {
+            audioPlayer.play()
+        }
         subscribeToModel()
         return binding.root
+    }
+
+    private fun createAudioPlayer(): ExoPlayer = ExoPlayer.Builder(requireContext())
+        .build().apply {
+            setMediaItem(MediaItem.fromUri("https://firebasestorage.googleapis.com/v0/b/time-mashine-minsk.appspot.com/o/a.mp3?alt=media&token=9a129ba3-4abb-42fa-b345-30544694375c"))
+            prepare()
     }
 
     override fun onResume() {
