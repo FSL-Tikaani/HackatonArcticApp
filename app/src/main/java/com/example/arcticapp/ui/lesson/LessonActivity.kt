4d6f370b9e5ec6c056e564c1cbb44855e683aed8
artemObrazumov <a.obraz60022006@gmail.com>
2023-01-28 22:50:25 +0300

    implemented tab system

--- app/src/main/java/com/example/arcticapp/ui/lesson/LessonActivity.kt
+++ app/src/main/java/com/example/arcticapp/ui/lesson/LessonActivity.kt
@@ -1,17 +1,51 @@
 package com.example.arcticapp.ui.lesson
 
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.example.arcticappfinal.R
 import com.example.arcticappfinal.databinding.ActivityLessonBinding
 
 class LessonActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLessonBinding
+    private lateinit var adapter: LessonTabAdapter
+    private lateinit var textColor: ColorStateList
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLessonBinding.inflate(layoutInflater)
-        binding.pager.adapter = LessonTabAdapter(this)
+        adapter = LessonTabAdapter(this)
+        binding.pager.adapter = adapter
+        binding.buttonTab1.setOnClickListener {
+            onTabClicked(0)
+        }
+        binding.buttonTab2.setOnClickListener {
+            onTabClicked(1)
+        }
+        textColor = binding.tabText1.textColors
         setContentView(binding.root)
+        onTabClicked(0)
+    }
+
+    private fun onTabClicked(position: Int) {
+        binding.pager.setCurrentItem(position, true)
+        if (position == 0) {
+            binding.buttonTab1.setBackgroundResource(R.drawable.tab_background_selected)
+            binding.buttonTab2.setBackgroundResource(R.drawable.tab_background)
+            binding.tabText1.setTextColor(
+                ContextCompat.getColor(this, R.color.white)
+            )
+            binding.tabText2.setTextColor(textColor)
+        }
+        if (position == 1) {
+            binding.buttonTab1.setBackgroundResource(R.drawable.tab_background)
+            binding.buttonTab2.setBackgroundResource(R.drawable.tab_background_selected)
+            binding.tabText1.setTextColor(textColor)
+            binding.tabText2.setTextColor(
+                ContextCompat.getColor(this, R.color.white)
+            )
+        }
     }
 }
