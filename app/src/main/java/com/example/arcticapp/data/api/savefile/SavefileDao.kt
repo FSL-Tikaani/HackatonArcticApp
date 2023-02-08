b2925ec5b99103f9894aa2deff7d192fe9757ead
artemObrazumov <a.obraz60022006@gmail.com>
2023-02-08 21:42:27 +0300

    dao class fix

---
+++ app/src/main/java/com/example/arcticapp/data/api/savefile/SavefileDao.kt
@@ -0,0 +1,34 @@
+package com.example.arcticapp.data.api.savefile
+
+import androidx.room.*
+import com.example.arcticapp.data.models.TaskResult
+
+@Dao
+interface SavefileDao {
+    @Query("SELECT * FROM taskresult")
+    fun getAllResults(): List<TaskResult>
+
+    @Query("SELECT * FROM taskresult WHERE taskID = :taskID")
+    fun getTaskResultByTaskID(taskID: String): TaskResult
+
+    @Query("SELECT * FROM taskresult WHERE taskID LIKE :lessonID")
+    fun getTaskResultsByLessonID(lessonID: String): TaskResult
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(result: TaskResult): Long
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun update(result: TaskResult): Int
+
+    @Transaction
+    suspend fun upsert(result: TaskResult): Long {
+        var id = insert(result)
+        if (id == -1L) {
+            id = update(result).toLong()
+        }
+        return id
+    }
+
+    @Delete
+    suspend fun delete(result: TaskResult)
+}
